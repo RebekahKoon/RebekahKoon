9b53b19cc44fe8c751146dc9b20a85864541794a
Rebekah <koonr@oregonstate.edu>
2021-06-22 17:31:30 -0700

    delete unused code

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -74,25 +74,6 @@ const SubmitButton = styled(ButtonBorder)`
   }
 `
 
-// const SubmitButton = styled.input.attrs({ type: 'submit' })`
-//   background: #00aec9;
-//   color: #fff;
-//   cursor: pointer;
-//   margin-bottom: 0;
-//   text-transform: uppercase;
-//   width: 100%;
-//   border-radius: 5px;
-//   height: 35px;
-//   border-color: transparent;
-//   box-shadow: 0px;
-//   outline: none;
-//   transition: 0.15s;
-//   text-align: center;
-//   &:active {
-//     background-color: #f1ac15;
-//   }
-// `
-
 const Contact = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
